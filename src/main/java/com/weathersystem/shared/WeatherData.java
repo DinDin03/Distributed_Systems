f6fd068eb861dfa5536fe1bad10d64cea16dc37e
DinDin03 <dineth.himasha@outlook.com>
2025-09-11 18:41:50 +0930

    WeatherData class created with lombok annotations for clean code

--- src/main/java/com/weathersystem/shared/WeatherData.java
+++ src/main/java/com/weathersystem/shared/WeatherData.java
@@ -1,4 +1,29 @@
 package com.weathersystem.shared;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class WeatherData {
+    private String id;
+    private String name;
+    private String state;
+    private String timeZone;
+    private double lat;
+    private double lon;
+    private String localDateTime;
+    private String localDateTimeFull;
+    private double airTemp;
+    private double apparentT;
+    private String cloud;
+    private double dewpt;
+    private double press;
+    private int relHum;
+    private String windDir;
+    private int windSpdKmh;
+    private int windSpdKt;
 }
